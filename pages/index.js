93e5a5b5964317b56394f1ed9baa0766e6111141
JeanCharles Cousin <cousin.jeanch@gmail.com>
2023-02-03 12:57:47 +0100

    onglet icon add

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
 import HomeFra from '../components/HomeFra';
 
 function Index() {
-  return <HomeFra />;
+  return (
+    <div>
+      <Head>
+        <title>News Of The World</title>
+        <link rel="icon" href="/icon_News.png" />
+      </Head>
+      <HomeFra />
+    </div>
+  );
 }
 
 export default Index;
